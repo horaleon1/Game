29d4bfc0b27643b1dee592d3e1bb7b951ea2c169
horaleon1 <horaleon1@gmail.com>
2019-05-28 20:10:22 -0500

    dibujando

--- src.js
+++ src.js
@@ -1,19 +1,58 @@
+//Variables
+var canvas = document.getElementById('canvas'), context = canvas.getContext('2d');
 
-var canvas1 = document.getElementById('canvas1'),context1 = canvas1.getContext('2d');
-
-canvas1.width = window.innerWidth;
-canvas1.height = 500;
-
+canvas.width = window.innerWidth;
+canvas.height = 500;
+//Clases
 class Board{
   constructor(){
-    this.x1 = 0;
-    this.y1 = 0;
-    this.w1= canvas1.width;
-    this.h1 = canvas1.height;
+    this.x = 0;
+    this.y = 0;
+    this.w= canvas.width;
+    this.h = canvas.height;
   }
 
-  draw1(){
-   canvas1.fillRect(0,0,canva1.width,canvas1.height);
+  draw(){
+   canvas.fillRect(0,0,canva.width,canvas.height);
   }
-
+  
 }
+
+var board = new Board();
+
+console.log(board.draw)
+
+class Ninja{
+  constructor(){
+   this.x = 50;
+   this.y = 50;
+   this.w = 50;
+   this.h = 40;
+   this.velX = 0;
+   this.velY = 0;
+   this.jumping = true;
+   
+  }
+  draw(){
+    context.fillStyle = "rgb(200,0,200)";
+    context.fillRect(this.x,this.y,this.w,this.h);
+  }
+  jump
+}
+
+var ninja = new Ninja();
+
+function update() {
+  ninja.draw();
+}
+
+update()
+
+
+
+//listeners
+// window.addEventListener('keydown', element =>{
+
+//   if (element.keyCode == 32) 
+
+// });
