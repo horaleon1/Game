57b0426404271aed1b4576b5d62c1b1363f4cb4d
horaleon1 <horaleon1@gmail.com>
2019-06-02 19:57:05 -0500

    "starting sprites"

--- src.js
+++ src.js
@@ -1,8 +1,12 @@
 //Variables
 var canvas = document.getElementById("canvas"),
   context = canvas.getContext("2d"),
-  cronometro;
-gravedad = 4;
+  cronometro,
+  gravedad = 4,
+  frames = 0,
+  currentFrame = 0,
+  arrayattack1 =[];
+  
 
 canvas.width = window.innerWidth;
 canvas.height = 500;
@@ -21,17 +25,25 @@ class Board {
 }
 
 class Ninja {
-  constructor(x, y, w, h) {
+  constructor(x, y, w, h,mx,my,mw,mh) {
+    this.mx = mx;
+    this.my = my;
+    this.mw = mw;
+    this.mh = mh
+    
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
     this.velX = 0;
     this.velY = 0;
+
+    this.img = new Image();
+    this.img.src = "./assets/img/Ninja-Run.png";
+    this.img.onload = this.draw();
   }
   draw() {
-    context.fillStyle = "rgb(200,0,200)";
-    context.fillRect(this.x, this.y, this.w, this.h);
+    context.drawImage(this.img,this.mx,this.my,this.mw,this.mh, this.x, this.y, this.w, this.h);
   }
 
   gravity() {
@@ -62,14 +74,28 @@ class Attack1 {
     this.y = y;
     this.w = w;
     this.h = h;
+
+    this.img = new Image();
+    this.img.src = "./assets/img/ROCKET0000.png"
+    this.img.onload = this.draw();
   }
   draw() {
-    context.fillStyle = "rgb(30,144,255)";
-    context.fillRect(this.x, this.y, this.w, this.h);
+    // context.fillStyle = "rgb(30,144,255)";
+    // context.fillRect(this.x, this.y, this.w, this.h);
+    context.drawImage(this.img,this.x,this.y,this.w,this.h);
   }
   moveAttack() {
     if (this.x < 950) this.x -= 400;
   }
+}
+
+function generateAttack1(){
+  arrayattack1.push(new Attack1(10,10,50,90));
+}
+function drawAttack1(){
+  arrayattack1.forEach(function(attack,i){
+    attack.attack1.draw();
+  })
 }
 
 class Attack2 {
@@ -88,10 +114,21 @@ class Attack2 {
   }
 }
 
+class Attack3 {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+}
+
 var board = new Board(),
-    ninja = new Ninja(10, 450, 50, 50),
-    attack1 = new Attack1(400, 450, 50, 50),
+    ninja = new Ninja(10, 450, 600/10, 200, 0, 0, 5000/10, 600),
+    //cohete
+    attack1 = new Attack1(400, 450, 90, 50),
     attack2 = new Attack2(800, 50, 50, 50);
+ 
 
 function update() {
   // crea
@@ -104,11 +141,19 @@ function update() {
   attack1.draw();
   //ataque2
   attack2.draw();
+  if (frames % 7 == 0){
+    currentFrame == ++ currentFrame % 3;
+  }
+  frames++;
 }
 
-setInterval(() => {
-  update();
-}, 1000 / 60);
+function start(){
+ 
+inicio = setInterval(update, 1000 / 60);
+
+}
+
+
 
 window.addEventListener("keydown", e => {
   if (e.keyCode === 39) ninja.moveRight();
@@ -123,63 +168,62 @@ window.addEventListener("keydown", e => {
 //botones de ataques
 //boton1
 let countAttack1 = document.getElementById("buttonAttack1"),
-    buttonAttack1 = document.getElementById("button1"),
-    conteo1 = 5;
+  buttonAttack1 = document.getElementById("button1"),
+  conteo1 = 5;
 
-function menosUno(){
-  if ( conteo1 > 0) conteo1--;
-    countAttack1.innerHTML = conteo1;
+function menosUno() {
+  if (conteo1 > 0) conteo1--;
+  countAttack1.innerHTML = conteo1;
 }
-buttonAttack1.addEventListener("click", e =>{
-   attack1.moveAttack();
-   menosUno();
+buttonAttack1.addEventListener("click", e => {
+  attack1.moveAttack();
+  menosUno();
 });
 
 //boton2
 let countAttack2 = document.getElementById("buttonAttack2"),
-    buttonAttack2 = document.getElementById("button2"),
-    conteo2 = 3;
+  buttonAttack2 = document.getElementById("button2"),
+  conteo2 = 3;
 
-function menosUno2(){
-  if ( conteo2 > 0) conteo2--;
-     countAttack2.innerHTML = conteo2;
+function menosUno2() {
+  if (conteo2 > 0) conteo2--;
+  countAttack2.innerHTML = conteo2;
 }
-buttonAttack2.addEventListener("click", e =>{
-   attack2.moveDown();
-   menosUno2();
+buttonAttack2.addEventListener("click", e => {
+  attack2.moveDown();
+  menosUno2();
 });
 //boton3
 let countAttack3 = document.getElementById("buttonAttack3"),
-    buttonAttack3 = document.getElementById("button3"),
-    conteo3 = 3;
+  buttonAttack3 = document.getElementById("button3"),
+  conteo3 = 3;
 
-function menosUno3(){
-  if ( conteo3 > 0) conteo3--;
-     countAttack3.innerHTML = conteo3;
+function menosUno3() {
+  if (conteo3 > 0) conteo3--;
+  countAttack3.innerHTML = conteo3;
 }
-buttonAttack3.addEventListener("click", e =>{
-   
-   menosUno3();
+buttonAttack3.addEventListener("click", e => {
+  menosUno3();
 });
 //boton4
 let countAttack4 = document.getElementById("buttonAttack4"),
-    buttonAttack4 = document.getElementById("button4"),
-    conteo4 = 10;
+  buttonAttack4 = document.getElementById("button4"),
+  conteo4 = 10;
 
-function menosUno4(){
-  if ( conteo4 > 0) conteo4--;
-     countAttack4.innerHTML = conteo4;
+function menosUno4() {
+  if (conteo4 > 0) conteo4--;
+  countAttack4.innerHTML = conteo4;
 }
-buttonAttack4.addEventListener("click", e =>{
-   
-   menosUno4();
+buttonAttack4.addEventListener("click", e => {
+  menosUno4();
 });
 
 //countdowns
-let seg1 = 75,seg2 = 75,
-    timer1 = document.getElementById("timer1"),
-    timer2 = document.getElementById("timer2"),
-    auxTimer = "";
+let seg1 = 75,
+  seg2 = 75,
+  timer1 = document.getElementById("timer1"),
+  timer2 = document.getElementById("timer2"),
+  auxTimer = "";
 
 function countdown1() {
   cronometro1 = setInterval(() => {
@@ -204,3 +248,5 @@ function countdown2() {
   }, 200);
 }
 countdown2();
+
+start();
